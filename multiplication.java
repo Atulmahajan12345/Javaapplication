0ff6231e10b54506ab2b3443002d705390a6a1d4
Atulmahajan12345 <atulmahajan2299@g,ail.com>
2022-03-17 17:51:19 +0530

    first commit

---
+++ multiplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class multiplication {
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Entre first value:");
+		int num1=sc.nextInt();
+		System.out.print("Entre second value:");
+		int num2=sc.nextInt();
+		int multiplication=num1*num2;
+		System.out.print("Multiplication of two numbers:"+multiplication);
+		
+
+}
+}
---
+++ username.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class username {
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.print("entre usernamne-");
+		String abc=sc.nextLine();
+		System.out.print("your name "+abc);
+	}
+}
+
+
